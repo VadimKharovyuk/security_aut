065b2ecc5695280406f78f05b35b9d70d3d48ae3
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-01-11 05:01:42 +0530

    fiter

---
+++ src/main/java/com/example/security_aut/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.security_aut.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService (PasswordEncoder encoder){
+       UserDetails admin = User.builder().username("admin").password(encoder.encode("admin")).build() ;
+       UserDetails user = User.builder().username("user").password(encoder.encode("user")).build() ;
+       UserDetails adwinit32 = User.builder().username("shaponline1993").password(encoder.encode("shaponline1993")).build() ;
+
+       return new InMemoryUserDetailsManager(admin,user,adwinit32);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(aut-> aut.requestMatchers("api/v1/apps/welcome").permitAll()
+                        .requestMatchers("api/v1/apps/**").authenticated())
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .build();
+
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
